b1bbad240020562c3db4741d792499b44ed5aa41
rened02 <renedlosada02@gmail.com>
2024-12-08 01:00:05 -0500

    adelantado hasta el capitulo 13 del tutorial
    YA ESTA:
    Principal
    Facturas ( Buscador,Tabla,Paginacion y CRUD)

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
- 
+export const experimental_ppr = true; // renderizará previamente las partes estáticas de la ruta y aplazará las partes dinámicas hasta que el usuario las solicite.
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
